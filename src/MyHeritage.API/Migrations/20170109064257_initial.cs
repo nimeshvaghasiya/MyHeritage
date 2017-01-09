72300c7f35e640ee4cb56488c93c779725143ed8
nimeshvaghasiya <nimesh.vaghasiya95@live.com>
2017-01-08 23:27:52 -0800

    add database migration

---
+++ src/MyHeritage.API/Migrations/20170109064257_initial.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace MyHeritage.API.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TreeMemberHeritages",
+                columns: table => new
+                {
+                    TreeMemberId = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AddressLine1 = table.Column<string>(nullable: true),
+                    AddressLine2 = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: true),
+                    BirthPlace = table.Column<string>(nullable: true),
+                    BirthlinktoCountryMasterId = table.Column<int>(nullable: true),
+                    BusinessAddress = table.Column<string>(nullable: true),
+                    BusinessCity = table.Column<string>(nullable: true),
+                    BusinessEmail = table.Column<string>(nullable: true),
+                    BusinessName = table.Column<string>(nullable: true),
+                    BusinessPhone = table.Column<string>(nullable: true),
+                    BusinessState = table.Column<string>(nullable: true),
+                    BusinessZip = table.Column<string>(nullable: true),
+                    CauseOfDeath = table.Column<string>(nullable: true),
+                    Cell = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<int>(nullable: true),
+                    DeceasedDate = table.Column<DateTime>(nullable: true),
+                    DeceasedPlace = table.Column<string>(nullable: true),
+                    DoNotEmailFromEPS = table.Column<string>(nullable: true),
+                    DoNotPrint = table.Column<bool>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    EmailFromVS = table.Column<string>(nullable: true),
+                    FatherName = table.Column<string>(nullable: true),
+                    Fax = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    Gender = table.Column<string>(nullable: true),
+                    GrandFatherName = table.Column<string>(nullable: true),
+                    HomePhone = table.Column<string>(nullable: true),
+                    Industry = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    IsApproxBirthdate = table.Column<bool>(nullable: false),
+                    IsBoard = table.Column<bool>(nullable: true),
+                    IsDeceased = table.Column<bool>(nullable: false),
+                    IsDisplayImage = table.Column<bool>(nullable: false),
+                    IsDoNotPrint1 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2007 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2008 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2009 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2010 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2011 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2012 = table.Column<bool>(nullable: true),
+                    IsDuesPaid2013 = table.Column<bool>(nullable: true),
+                    IsLifeMember = table.Column<bool>(nullable: true),
+                    IsReplaceWithMember = table.Column<bool>(nullable: true),
+                    LastModifiedBy = table.Column<int>(nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    LinkToMemberMasterId = table.Column<int>(nullable: false),
+                    LinktoStateMasterId = table.Column<int>(nullable: true),
+                    MainComments = table.Column<string>(nullable: true),
+                    MaritalStatus = table.Column<string>(nullable: true),
+                    MaternalGrandFather = table.Column<string>(nullable: true),
+                    MaternalGrandMother = table.Column<string>(nullable: true),
+                    MemberImage = table.Column<string>(nullable: true),
+                    MemberStatus = table.Column<char>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    Mother = table.Column<string>(nullable: true),
+                    MothersVillage = table.Column<string>(nullable: true),
+                    NodeId = table.Column<long>(nullable: false),
+                    Occupation = table.Column<string>(nullable: true),
+                    ParentIdentityId = table.Column<int>(nullable: false),
+                    PhysicalImage = table.Column<string>(nullable: true),
+                    RelationId = table.Column<int>(nullable: false),
+                    SiteUserId = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Village = table.Column<string>(nullable: true),
+                    WhoUpdated = table.Column<string>(nullable: true),
+                    ZipCode = table.Column<string>(nullable: true),
+                    callstatus = table.Column<char>(nullable: true),
+                    linktoCountryMasterId = table.Column<int>(nullable: true),
+                    linktoGamDetailId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreeMemberHeritages", x => x.TreeMemberId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TreeRelationMaster",
+                columns: table => new
+                {
+                    RelationId = table.Column<short>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrderId = table.Column<int>(nullable: false),
+                    RelationName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreeRelationMaster", x => x.RelationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TreeMemberHeritages");
+
+            migrationBuilder.DropTable(
+                name: "TreeRelationMaster");
+        }
+    }
+}
---
+++ src/MyHeritage.API/Migrations/MyHeritageContextModelSnapshot.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MyHeritage.Data;
+
+namespace MyHeritage.API.Migrations
+{
+    [DbContext(typeof(MyHeritageContext))]
+    partial class MyHeritageContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("MyHeritage.Model.Entity.TreeMemberHeritage", b =>
+                {
+                    b.Property<long>("TreeMemberId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("AddressLine1");
+
+                    b.Property<string>("AddressLine2");
+
+                    b.Property<DateTime?>("BirthDate");
+
+                    b.Property<string>("BirthPlace");
+
+                    b.Property<int?>("BirthlinktoCountryMasterId");
+
+                    b.Property<string>("BusinessAddress");
+
+                    b.Property<string>("BusinessCity");
+
+                    b.Property<string>("BusinessEmail");
+
+                    b.Property<string>("BusinessName");
+
+                    b.Property<string>("BusinessPhone");
+
+                    b.Property<string>("BusinessState");
+
+                    b.Property<string>("BusinessZip");
+
+                    b.Property<string>("CauseOfDeath");
+
+                    b.Property<string>("Cell");
+
+                    b.Property<string>("City");
+
+                    b.Property<DateTime?>("CreateDate");
+
+                    b.Property<int?>("CreatedBy");
+
+                    b.Property<DateTime?>("DeceasedDate");
+
+                    b.Property<string>("DeceasedPlace");
+
+                    b.Property<string>("DoNotEmailFromEPS");
+
+                    b.Property<bool?>("DoNotPrint");
+
+                    b.Property<string>("Email");
+
+                    b.Property<string>("EmailFromVS");
+
+                    b.Property<string>("FatherName");
+
+                    b.Property<string>("Fax");
+
+                    b.Property<string>("FirstName");
+
+                    b.Property<string>("Gender");
+
+                    b.Property<string>("GrandFatherName");
+
+                    b.Property<string>("HomePhone");
+
+                    b.Property<string>("Industry");
+
+                    b.Property<bool>("IsActive");
+
+                    b.Property<bool>("IsApproxBirthdate");
+
+                    b.Property<bool?>("IsBoard");
+
+                    b.Property<bool>("IsDeceased");
+
+                    b.Property<bool>("IsDisplayImage");
+
+                    b.Property<bool?>("IsDoNotPrint1");
+
+                    b.Property<bool?>("IsDuesPaid2007");
+
+                    b.Property<bool?>("IsDuesPaid2008");
+
+                    b.Property<bool?>("IsDuesPaid2009");
+
+                    b.Property<bool?>("IsDuesPaid2010");
+
+                    b.Property<bool?>("IsDuesPaid2011");
+
+                    b.Property<bool?>("IsDuesPaid2012");
+
+                    b.Property<bool?>("IsDuesPaid2013");
+
+                    b.Property<bool?>("IsLifeMember");
+
+                    b.Property<bool?>("IsReplaceWithMember");
+
+                    b.Property<int?>("LastModifiedBy");
+
+                    b.Property<DateTime?>("LastModifiedDate");
+
+                    b.Property<string>("LastName");
+
+                    b.Property<int>("LinkToMemberMasterId");
+
+                    b.Property<int?>("LinktoStateMasterId");
+
+                    b.Property<string>("MainComments");
+
+                    b.Property<string>("MaritalStatus");
+
+                    b.Property<string>("MaternalGrandFather");
+
+                    b.Property<string>("MaternalGrandMother");
+
+                    b.Property<string>("MemberImage");
+
+                    b.Property<char?>("MemberStatus");
+
+                    b.Property<string>("MiddleName");
+
+                    b.Property<string>("Mother");
+
+                    b.Property<string>("MothersVillage");
+
+                    b.Property<long>("NodeId");
+
+                    b.Property<string>("Occupation");
+
+                    b.Property<int>("ParentIdentityId");
+
+                    b.Property<string>("PhysicalImage");
+
+                    b.Property<int>("RelationId");
+
+                    b.Property<string>("SiteUserId");
+
+                    b.Property<string>("State");
+
+                    b.Property<string>("Village");
+
+                    b.Property<string>("WhoUpdated");
+
+                    b.Property<string>("ZipCode");
+
+                    b.Property<char?>("callstatus");
+
+                    b.Property<int?>("linktoCountryMasterId");
+
+                    b.Property<int>("linktoGamDetailId");
+
+                    b.HasKey("TreeMemberId");
+
+                    b.ToTable("TreeMemberHeritages");
+                });
+
+            modelBuilder.Entity("MyHeritage.Model.Entity.TreeRelationMaster", b =>
+                {
+                    b.Property<short>("RelationId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("OrderId");
+
+                    b.Property<string>("RelationName");
+
+                    b.HasKey("RelationId");
+
+                    b.ToTable("TreeRelationMaster");
+                });
+        }
+    }
+}
